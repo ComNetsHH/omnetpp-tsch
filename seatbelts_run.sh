07800d2959c3798d19b69a6839014c8af22af657
Yevhenii Shudrenko <yevhenii.shudrenko@tuhh.de>
2021-08-21 23:54:15 +0200

    adjusted run scripts

--- seatbelts_run.sh
+++ seatbelts_run.sh
@@ -1,1 +1,1 @@
-opp_runall -j16 src/tsch-master -u Cmdenv -c SeatbeltStatus -r '$sinks < 4 && $hosts == 200' -n simulations:src:${HOME}/omnetpp-5.6.2/samples/omnetpp-rpl/src:${HOME}/omnetpp-5.6.2/samples/inet/src simulations/wireless/waic/omnetpp.ini
+opp_runall -j16 src/tsch-master -u Cmdenv -c SeatbeltStatus -r '$hosts == 200' -n simulations:src:${HOME}/omnetpp-5.6.2/samples/omnetpp-rpl/src:${HOME}/omnetpp-5.6.2/samples/inet4/src simulations/wireless/waic/omnetpp.ini
