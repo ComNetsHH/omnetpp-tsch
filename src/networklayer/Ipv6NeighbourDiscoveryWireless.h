5f5552beb3148dc3e5b448246cd90571d60ef231
Yevhenii Shudrenko <yevhenii.shudrenko@tuhh.de>
2021-10-06 15:14:09 +0200

    migrated all modified parts of IPv6 ND to our own Wireless Neighbor Discovery (WiND)

--- src/networklayer/Ipv6NeighbourDiscoveryWireless.h
+++ src/networklayer/Ipv6NeighbourDiscoveryWireless.h
@@ -25,26 +25,93 @@ using namespace inet;
 
 namespace tsch {
 
+#define WIND_SEND_DELAYED                  8 // self-msg kind, see Ipv6NeighbourDiscovery.h
+
+// Minor delays added for EACH packet forwarded to IP layer
+#define WIND_MICRO_DELAY_MIN               0.3
+#define WIND_MICRO_DELAY_MAX               0.5
+
 class Ipv6NeighbourDiscoveryWireless : public Ipv6NeighbourDiscovery
 {
 protected:
+    class Ipv6NdPacketInfo : public cObject {
+        public:
+            Ipv6NdPacketInfo() {}
+            Ipv6NdPacketInfo(Packet *pkt, Ipv6Address destAddr, Ipv6Address srcAddr, int ie)
+            {
+                this->msgPtr = pkt;
+                this->destAddr = destAddr;
+                this->srcAddr = srcAddr;
+                this->interfaceId = ie;
+            }
+
+            const Ipv6Address& getDestAddr() const {
+                return destAddr;
+            }
+
+            void setDestAddr(const Ipv6Address &destAddr) {
+                this->destAddr = destAddr;
+            }
+
+            int getInterfaceId() const {
+                return interfaceId;
+            }
+
+            void setInterfaceId(int interfaceId) {
+                this->interfaceId = interfaceId;
+            }
+
+            Packet*& getMsgPtr() {
+                return msgPtr;
+            }
+
+            void setMsgPtr(Packet *&msgPtr) {
+                this->msgPtr = msgPtr;
+            }
+
+            const Ipv6Address& getSrcAddr() const {
+                return srcAddr;
+            }
+
+            void setSrcAddr(const Ipv6Address &srcAddr) {
+                this->srcAddr = srcAddr;
+            }
+
+        private:
+            Packet *msgPtr;
+            Ipv6Address destAddr;
+            Ipv6Address srcAddr;
+            int interfaceId;
+    };
+
     virtual void initialize(int stage) override;
-    virtual void createAndSendNsPacket(const Ipv6Address& nsTargetAddr, const Ipv6Address& dgDestAddr,
-                const Ipv6Address& dgSrcAddr, InterfaceEntry *ie) override;
 
     virtual void sendSolicitedNa(Packet *packet,
             const Ipv6NeighbourSolicitation *ns, InterfaceEntry *ie) override;
 
     virtual void sendUnsolicitedNa(InterfaceEntry *ie) override;
     virtual void createAndSendRaPacket(const Ipv6Address& destAddr, InterfaceEntry *ie) override;
+    virtual void createAndSendNsPacket(const Ipv6Address& nsTargetAddr, const Ipv6Address& dgDestAddr,
+                    const Ipv6Address& dgSrcAddr, InterfaceEntry *ie) override;
 
     virtual void createRaTimer(InterfaceEntry *ie) override;
+    virtual void sendPacketToIpv6Module(Packet *msg, const Ipv6Address& destAddr,
+            const Ipv6Address& srcAddr, int interfaceId, double delay);
 
+    virtual void handleMessage(cMessage *msg) override;
+    virtual void initiateNeighbourUnreachabilityDetection(Neighbour *nce) override;
+    virtual void processNaForIncompleteNceState(const Ipv6NeighbourAdvertisement *na, Neighbour *nce) override;
+    virtual void processIpv6Datagram(Packet *packet) override;
+    virtual void processNaForOtherNceStates(const Ipv6NeighbourAdvertisement *na, Neighbour *nce) override;
 
     simsignal_t naSolicitedPacketSent;
     simsignal_t naUnsolicitedPacketSent;
     simsignal_t nsPacketSent;
     simsignal_t raPacketSent;
+    simsignal_t nudInitated;
+
+    double nsFwdDelay;
+    bool pAddRandomDelays; // add a miniscule delay before sending ANY packet out, prevents the infinite loop bug
 };
 
 }
