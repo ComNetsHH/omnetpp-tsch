f63da9a34df418ea3580aace45f653ced5b48c04
Yevhenii Shudrenko <yevhenii.shudreko@tuhh.de>
2022-03-15 14:12:52 +0100

    adjusted CLX paper run scripts

--- smoke_humidity_run.sh
+++ smoke_humidity_run.sh
@@ -1,1 +1,1 @@
-opp_runall -j16 src/tsch-master -u Cmdenv -c SmokeHumidity -n simulations:src:${HOME}/omnetpp-5.6.2/samples/omnetpp-rpl/src:${HOME}/omnetpp-5.6.2/samples/inet4/src simulations/wireless/waic/omnetpp.ini
+opp_runall -j16 src/tsch -u Cmdenv -c SmokeHumidity -n simulations:src:${HOME}/omnetpp-5.6.2/samples/omnetpp-rpl/src:${HOME}/omnetpp-5.6.2/samples/inet4/src simulations/wireless/waic/omnetpp.ini
